6767ccc7508e3357018a3eaf0b74af4215ad95d0
Saul Rowntree <srowntree03@qub.ac.uk>
2022-03-28 10:10:49 +0100

    Added all tests

--- org/jfree/data/test/DataUtilitiesTest.java
+++ org/jfree/data/test/DataUtilitiesTest.java
@@ -4,7 +4,9 @@ import java.security.InvalidParameterException;
 import java.util.Arrays;
 
 import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
 import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.KeyedValues;
 import org.jfree.data.Values2D;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -18,7 +20,7 @@ public class DataUtilitiesTest extends TestCase {
 
 	private Values2D values2D;
 	DefaultKeyedValues2D validTestValues;
-	DefaultKeyedValues2D emptyValues = new DefaultKeyedValues2D();;
+	DefaultKeyedValues2D emptyValues = new DefaultKeyedValues2D();
 
 	public void setUp() {
 		validTestValues = new DefaultKeyedValues2D();
@@ -59,17 +61,17 @@ public class DataUtilitiesTest extends TestCase {
 	@Test
 	public void testCalculateColumnTotalTC1() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateColumnTotal(values2D,  3), 499.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateColumnTotal(values2D, 3), 499.0);
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalTC2() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateColumnTotal(values2D,  12), 0.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateColumnTotal(values2D, 12), 0.0);
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalTC3() {
 		try {
@@ -79,7 +81,7 @@ public class DataUtilitiesTest extends TestCase {
 			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalTC4() {
 		try {
@@ -93,34 +95,34 @@ public class DataUtilitiesTest extends TestCase {
 	@Test
 	public void testCalculateColumnTotalTC5() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateColumnTotal(emptyValues,  0), 0.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateColumnTotal(emptyValues, 0), 0.0);
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalTC6() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateColumnTotal(emptyValues,  -452), 0.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateColumnTotal(emptyValues, -452), 0.0);
 	}
-	
+
 	// 2 //
 	// CalculateRowTotal
 
 	@Test
 	public void testCalculateRowTotalTC1() {
 		values2D = validTestValues;
-		assertEquals("calculateRowTotal: Did not return the expected output.", 
-				DataUtilities.calculateRowTotal(values2D,  0), 105.0);
+		assertEquals("calculateRowTotal: Did not return the expected output.",
+				DataUtilities.calculateRowTotal(values2D, 0), 105.0);
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalTC2() {
 		values2D = validTestValues;
-		assertEquals("calculateRowTotal: Did not return the expected output.", 
-				DataUtilities.calculateRowTotal(values2D,  3), 0);
+		assertEquals("calculateRowTotal: Did not return the expected output.",
+				DataUtilities.calculateRowTotal(values2D, 3), 0);
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalTC3() {
 		try {
@@ -130,7 +132,7 @@ public class DataUtilitiesTest extends TestCase {
 			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalTC4() {
 		try {
@@ -144,16 +146,126 @@ public class DataUtilitiesTest extends TestCase {
 	@Test
 	public void testCalculateRowTotalTC5() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateRowTotal(emptyValues,  0), 0.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateRowTotal(emptyValues, 0), 0.0);
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalTC6() {
 		values2D = validTestValues;
-		assertEquals("calculateColumnTotal: Did not return the expected output.", 
-				DataUtilities.calculateRowTotal(emptyValues,  -452), 0.0);
+		assertEquals("calculateColumnTotal: Did not return the expected output.",
+				DataUtilities.calculateRowTotal(emptyValues, -452), 0.0);
+	}
+
+	// 3 //
+	// CreateNumberArray
+
+	@Test
+	public void testCreateNumberArrayTC1() {
+		double[] doubles = { 1.0, 2.0, -1.0, -2.0, -1000.0, 45.0 };
+
+		Number[] nums = DataUtilities.createNumberArray(doubles);
+		assertEquals("createNumberArray: Index 0 did not contain the expected value.", doubles[0], nums[0]);
+		assertEquals("createNumberArray: Index 1 did not contain the expected value.", doubles[1], nums[1]);
+		assertEquals("createNumberArray: Index 2 did not contain the expected value.", doubles[2], nums[2]);
+		assertEquals("createNumberArray: Index 3 did not contain the expected value.", doubles[3], nums[3]);
+		assertEquals("createNumberArray: Index 4 did not contain the expected value.", doubles[4], nums[4]);
+		assertEquals("createNumberArray: Index 5 did not contain the expected value.", doubles[5], nums[5]);
+	}
+
+	@Test
+	public void testCreateNumberArrayTC2() {
+		try {
+			Number[] nums = DataUtilities.createNumberArray(null);
+			fail("No exception thrown-Expected outcome was: a thrown exception of type: InvalidParameterException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
+		}
+	}
+	
+	@Test
+	public void testCreateNumberArrayTC3() {
+		double[] empty = {};
+		Number[] nums = DataUtilities.createNumberArray(empty);
+		assertEquals("createNumberArray: Did not contain the expected output.", nums.length, 0);
+	}
+	
+	// 4 //
+	// CreateNumberArray2D
+	
+	@Test
+	public void testCreateNumberArray2DTC1() {
+		double[][] doubles = {{ 1.0, 2.0, 3.0},{-8.0, -43.0, 200.0}};
+
+		Number[][] nums = DataUtilities.createNumberArray2D(doubles);
+		assertEquals("createNumberArray2D: Index 0,0 did not contain the expected value.", doubles[0][0], nums[0][0]);
+		assertEquals("createNumberArray2D: Index 0,1 did not contain the expected value.", doubles[0][1], nums[0][1]);
+		assertEquals("createNumberArray2D: Index 0,2 did not contain the expected value.", doubles[0][2], nums[0][2]);
+		assertEquals("createNumberArray2D: Index 1,0 did not contain the expected value.", doubles[1][0], nums[1][0]);
+		assertEquals("createNumberArray2D: Index 1,1 did not contain the expected value.", doubles[1][1], nums[1][1]);
+		assertEquals("createNumberArray2D: Index 1,2 did not contain the expected value.", doubles[1][2], nums[1][2]);
+	}
+
+	@Test
+	public void testCreateNumberArray2DTC2() {
+		try {
+			Number[][] nums = DataUtilities.createNumberArray2D(null);
+			fail("No exception thrown-Expected outcome was: a thrown exception of type: InvalidParameterException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
+		}
+	}
+	
+	@Test
+	public void testCreateNumberArray2DTC3() {
+		double[][] empty = {{},{}};
+		Number[][] nums = DataUtilities.createNumberArray2D(empty);
+		assertEquals("createNumberArray2D: Did not contain the expected output.", nums.length, 2);
 	}
 	
 	
+	// 5 //
+	// GetCumulativePercentages
+	
+	@Test
+	public void testGetCumulativePercentagesTC1() {
+		DefaultKeyedValues keyedValues = new DefaultKeyedValues();
+        keyedValues.addValue((Comparable) 0, 6.0);
+        keyedValues.addValue((Comparable) 1, 11.0);
+        keyedValues.addValue((Comparable) 2, 3.0);
+        keyedValues.addValue((Comparable) 3, -4.0);
+        keyedValues.addValue((Comparable) 4, 1.0);
+        keyedValues.addValue((Comparable) 5, 9.0);
+        KeyedValues objectUnderTest = DataUtilities.getCumulativePercentages((KeyedValues) keyedValues);
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		0.23076923076923078, (double)objectUnderTest.getValue(0));
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		0.6538461538461539, (double)objectUnderTest.getValue(1));
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		0.7692307692307693, (double)objectUnderTest.getValue(2));
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		0.6153846153846154, (double)objectUnderTest.getValue(3));
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		0.6538461538461539, (double)objectUnderTest.getValue(4));
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",
+        		1.0, (double)objectUnderTest.getValue(5));
+	}
+	
+	@Test
+	public void testGetCumulativePercentagesTC2() {
+        try {
+        	DefaultKeyedValues keyedValues = null;
+            KeyedValues objectUnderTest = DataUtilities.getCumulativePercentages((KeyedValues) keyedValues);
+			fail("No exception thrown-Expected outcome was: a thrown exception of type: InvalidParameterException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
+		}
+	}
+	
+	@Test
+	public void testGetCumulativePercentagesTC3() {
+		DefaultKeyedValues keyedValues = new DefaultKeyedValues();
+        KeyedValues objectUnderTest = DataUtilities.getCumulativePercentages((KeyedValues) keyedValues);
+        assertEquals("getCumulativePercentages: Did not contain the expected output.",0,objectUnderTest.getItemCount());
+}
 }
